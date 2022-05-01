f690a4d396f3ff5101c87bda2faae914cc2b7363
Hamo Muradyan <hamomuradyan93@gmail.com>
2022-05-01 13:05:09 +0400

    grids

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 import 'bootstrap/dist/js/bootstrap.bundle';
 import Navbar1 from './components/Navbar1';
 import Carousel from './components/Carousel';
+import Container from './components/Container';
 
 function App() {
   return (
     <>
       <Navbar1 />
       <Carousel />
+      <Container />
     </>
   );
 }
---
+++ src/components/Container.js
@@ -0,0 +1,68 @@
+import React from 'react'
+
+const Container = () => {
+  return (
+    <div className="container-fluid my-2">
+        <div className='row'>
+            <div className='col-md-8 col-sm-8'>
+                <img className='w-100' src='https://scontent.fevn7-1.fna.fbcdn.net/v/t39.30808-6/278568625_5304557322939939_5101258044439837936_n.jpg?_nc_cat=105&ccb=1-5&_nc_sid=8bfeb9&_nc_ohc=IXwSkodAWa8AX-rT__r&_nc_ht=scontent.fevn7-1.fna&oh=00_AT-H0gVqQXuDPcB8jpCnxkK7R8S1EWjL9a3h2Pvc2UU3rw&oe=6272AF8D' alt='Opera' />
+            </div>
+            <div className='col-md-4 col-sm-4 text-center'>
+                <h2 className='my-3'>Hello from Opera!</h2>
+                <p>Lorem ipsum dolor sit amet consectetutus vel enim posuere. In nisi feugiat natoque dictum viverra enim lectus fringilla, facilisis ante sagittis dis litora volutpat conubia augue maecenas, ad suspendisse ultrices senectus leo aenean integer. Ut fermentum pharetra class rhoncus inceptos vitae in, netus ante dui sociosqu consequat suscipit felis porta, ultricies egestas parturient donec diam suspendisse. Hendrerit dictum senectus platea pharetra erat nullam arcu eleifend vestibulum duis habitasse, pellentesque conubia eros enim mattis porta et scelerisque nam nec, primis odio sociosqu nunc egestas hac nostra interdum sagittis elementum. Cras aliquam mattis at commodo turpis proin, nisl quisque mi a iaculis curae, nascetur morbi curabitur ridiculus habitant.</p>
+            </div>
+        </div>
+        <h2 className='text-center my-5'>Կայքի բլոկներ</h2>
+        <div className='row gy-5'>
+            <div className='col-md-4 col-sm-6 col-lg-2 text-center'>
+                <h4>Lorem Ipsum</h4>
+                <svg xmlns="http://www.w3.org/2000/svg" width="36" height="36" fill="currentColor" class="bi bi-images" viewBox="0 0 16 16">
+                    <path d="M4.502 9a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3z"/>
+                    <path d="M14.002 13a2 2 0 0 1-2 2h-10a2 2 0 0 1-2-2V5A2 2 0 0 1 2 3a2 2 0 0 1 2-2h10a2 2 0 0 1 2 2v8a2 2 0 0 1-1.998 2zM14 2H4a1 1 0 0 0-1 1h9.002a2 2 0 0 1 2 2v7A1 1 0 0 0 15 11V3a1 1 0 0 0-1-1zM2.002 4a1 1 0 0 0-1 1v8l2.646-2.354a.5.5 0 0 1 .63-.062l2.66 1.773 3.71-3.71a.5.5 0 0 1 .577-.094l1.777 1.947V5a1 1 0 0 0-1-1h-10z"/>
+                </svg>
+                <p>sit amet consectetur adipiscing elit, etiam integer maecenas duis in. Nunc at sapien morbi et quis mauris aliquet hendrerit nec pellentesque nisl</p>
+            </div>
+            <div className='col-md-4 col-sm-6 col-lg-2 text-center'>
+                <h4>Lorem Ipsum</h4>
+                <svg xmlns="http://www.w3.org/2000/svg" width="36" height="36" fill="currentColor" class="bi bi-info-circle" viewBox="0 0 16 16">
+                    <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
+                    <path d="m8.93 6.588-2.29.287-.082.38.45.083c.294.07.352.176.288.469l-.738 3.468c-.194.897.105 1.319.808 1.319.545 0 1.178-.252 1.465-.598l.088-.416c-.2.176-.492.246-.686.246-.275 0-.375-.193-.304-.533L8.93 6.588zM9 4.5a1 1 0 1 1-2 0 1 1 0 0 1 2 0z"/>
+                </svg>
+                <p>sit amet consectetur adipiscing elit, etiam integer maecenas duis in. Nunc at sapien morbi et quis mauris aliquet hendrerit nec pellentesque nisl</p>
+            </div>
+            <div className='col-md-4 col-sm-6 col-lg-2 text-center'>
+                <h4>Lorem Ipsum</h4>
+                <svg xmlns="http://www.w3.org/2000/svg" width="36" height="36" fill="currentColor" class="bi bi-house-door" viewBox="0 0 16 16">
+                    <path d="M8.354 1.146a.5.5 0 0 0-.708 0l-6 6A.5.5 0 0 0 1.5 7.5v7a.5.5 0 0 0 .5.5h4.5a.5.5 0 0 0 .5-.5v-4h2v4a.5.5 0 0 0 .5.5H14a.5.5 0 0 0 .5-.5v-7a.5.5 0 0 0-.146-.354L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293L8.354 1.146zM2.5 14V7.707l5.5-5.5 5.5 5.5V14H10v-4a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5v4H2.5z"/>
+                </svg>
+                <p>sit amet consectetur adipiscing elit, etiam integer maecenas duis in. Nunc at sapien morbi et quis mauris aliquet hendrerit nec pellentesque nisl</p>
+            </div>
+            <div className='col-md-4 col-sm-6 col-lg-2 text-center'>
+                <h4>Lorem Ipsum</h4>
+                <svg xmlns="http://www.w3.org/2000/svg" width="36" height="36" fill="currentColor" class="bi bi-images" viewBox="0 0 16 16">
+                    <path d="M4.502 9a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3z"/>
+                    <path d="M14.002 13a2 2 0 0 1-2 2h-10a2 2 0 0 1-2-2V5A2 2 0 0 1 2 3a2 2 0 0 1 2-2h10a2 2 0 0 1 2 2v8a2 2 0 0 1-1.998 2zM14 2H4a1 1 0 0 0-1 1h9.002a2 2 0 0 1 2 2v7A1 1 0 0 0 15 11V3a1 1 0 0 0-1-1zM2.002 4a1 1 0 0 0-1 1v8l2.646-2.354a.5.5 0 0 1 .63-.062l2.66 1.773 3.71-3.71a.5.5 0 0 1 .577-.094l1.777 1.947V5a1 1 0 0 0-1-1h-10z"/>
+                </svg>
+                <p>sit amet consectetur adipiscing elit, etiam integer maecenas duis in. Nunc at sapien morbi et quis mauris aliquet hendrerit nec pellentesque nisl</p>
+            </div>
+            <div className='col-md-4 col-sm-6 col-lg-2 text-center'>
+                <h4>Lorem Ipsum</h4>
+                <svg xmlns="http://www.w3.org/2000/svg" width="36" height="36" fill="currentColor" class="bi bi-info-circle" viewBox="0 0 16 16">
+                    <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
+                    <path d="m8.93 6.588-2.29.287-.082.38.45.083c.294.07.352.176.288.469l-.738 3.468c-.194.897.105 1.319.808 1.319.545 0 1.178-.252 1.465-.598l.088-.416c-.2.176-.492.246-.686.246-.275 0-.375-.193-.304-.533L8.93 6.588zM9 4.5a1 1 0 1 1-2 0 1 1 0 0 1 2 0z"/>
+                </svg>
+                <p>sit amet consectetur adipiscing elit, etiam integer maecenas duis in. Nunc at sapien morbi et quis mauris aliquet hendrerit nec pellentesque nisl</p>
+            </div>
+            <div className='col-md-4 col-sm-6 col-lg-2 text-center'>
+                <h4>Lorem Ipsum</h4>
+                <svg xmlns="http://www.w3.org/2000/svg" width="36" height="36" fill="currentColor" class="bi bi-house-door" viewBox="0 0 16 16">
+                    <path d="M8.354 1.146a.5.5 0 0 0-.708 0l-6 6A.5.5 0 0 0 1.5 7.5v7a.5.5 0 0 0 .5.5h4.5a.5.5 0 0 0 .5-.5v-4h2v4a.5.5 0 0 0 .5.5H14a.5.5 0 0 0 .5-.5v-7a.5.5 0 0 0-.146-.354L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293L8.354 1.146zM2.5 14V7.707l5.5-5.5 5.5 5.5V14H10v-4a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5v4H2.5z"/>
+                </svg>
+                <p>sit amet consectetur adipiscing elit, etiam integer maecenas duis in. Nunc at sapien morbi et quis mauris aliquet hendrerit nec pellentesque nisl</p>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Container;
